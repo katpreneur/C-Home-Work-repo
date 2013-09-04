db4fcd23cbfbbac8e9d51e21d85caf144882d72e
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-09-04 07:00:01 -0700

    Cleanup some format

--- HW1.c
+++ HW1.c
@@ -4,15 +4,10 @@ int main(int argc, char *argv[])
 {
 	char full_name[] = {'K','a','t','i','a',' ','S','u','c','h','k','o','v','a','\0'};
 
-	int i = 0;
-		for (i = 0; i <= 14; i++)
+  for (int i = 0; i <= sizeof(full_name)/sizeof(char)-2; i++)
 	{
 		printf("%c\n", full_name[i]);
 	}
-	return 0;
-	}
 
-//for(INITIALIZER; TEST; INCREMENTER) 
-	//{
-    //CODE;
-//}
+	return 0;
+}
